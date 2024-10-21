ebc70c27dc97c331b9f8ad002051f9d70055c117
Mita-12 <pandamadhumita43@gmail.com>
2024-10-21 12:53:58 +0530

    some add

---
+++ frontend/JAVASCRIPT/class6/iteration.js
@@ -0,0 +1,54 @@
+//print the no b/w 1to 5;
+//for(let i=1;i<=5;i++){
+   // console.log(i);
+//}
+
+//print 5 to 1
+//for(let i=5;i>=1;i--){
+   // console.log(i);
+    
+//}
+//print 20 to 10
+//for(let i=20;i>=10;i--){
+   // console.log(i);
+    
+//}
+//generate return number and the up to 5 times
+/*let sum=0;
+for(let i=1;i<=5;i++){
+    console.log(i);
+    i++;
+    
+}*/
+
+/*let sum=0;
+for(let i=1;i<=5;i++){
+    let random=Math.floor(Math.random()*10);
+    let result =sum+random;
+    console.log(`The sum of the random number is ${result}`);
+}*/
+
+/*for(let i=1;i<=5;i++){
+    for(let j=1;j<=2;j++){
+        console.log(j);   
+    }
+    console.log(i);  
+}*/
+
+//pattren printing
+/*for(let i=1;i<=5;i++){
+    let pattern="";
+    for(let j=1;j<=i;j++){
+        pattern+="*";  
+    }
+    console.log(pattern);
+}*/
+//Revers pattern
+for(let i=5;i>=1;i--){
+   let pattern="";
+   for(let j=1;j>=i;j++){
+    pattern+="*";
+   }
+   console.log(pattern);
+   
+}
---
+++ frontend/JAVASCRIPT/class6/note.md
@@ -0,0 +1,20 @@
+For loop
+syntax = for(initilization;condition;invrement/decrement){
+    code be executed
+}
+Example1;
+print the no b/w 1to 5;
+for(let i=0;i<5;i++){
+    console.log(i)
+}
+
+Nested loop 
+Example:
+pattren printing
+for(let i=1;i<=5;i++){
+    let pattern="";
+    for(let j=1;j<=;j++){
+        pattern="*";      
+    }
+    consile.log(pattern);
+}
