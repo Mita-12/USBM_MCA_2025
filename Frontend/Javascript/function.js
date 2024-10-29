71d4f62d1593e100022f1f937b67c61eddf7364a
John DoeMita_12 <pandamadhumita43@gmail.com>
2024-10-29 23:04:23 +0530

    add the class

---
+++ Frontend/Javascript/function.js
@@ -0,0 +1,51 @@
+//Function
+//write a function to print your name
+/*function printNmae(){
+    console.log(`My name is ${"Madhu"}`);
+}
+printNmae();
+*/
+/*function printName(){
+    console.log("Madhu");
+}
+printName();
+
+// Pass the Parameter and Argument
+function printName(name){
+    console.log(`My name is ${"name"}`);
+}
+printName("Madhumita");
+
+function printName(name,age,city){
+    console.log(`My name is ${"Madhu"} age${25} city${"BBSR"}`);
+}
+printName();
+
+*/
+//Use in Return key word
+/*
+function printName(name) {
+    return `My name is ${"Madhu"}`;
+}
+printName();
+
+function printName(name) {
+   let output= printName("Madhu");
+   console.log(output);
+
+}
+printName();
+
+//Tow time print 
+printName();
+function printName(name) {
+    console.log(`My name is ${"madhu"}`);
+}
+printName();
+*/
+//Arrow function
+const printNmae=(name)=>
+{
+    return`My name is ${"name"}`;
+}
+console.log(printName("madhu"));// show the reference error
